93a5a4cbdc695fbc712a0142fcc4d7f359feefb3
Alejandro Santiago <alemures@hotmail.com>
2026-02-20 13:59:20 +0100

    Using volumes in lowercase

--- scripts/set-data-permissions.sh
+++ scripts/set-data-permissions.sh
@@ -2,6 +2,6 @@
 
 DISK="/srv/dev-disk-by-uuid-c497dd5c-e3cc-48fb-b67c-a6cb0865cf53"
 
-sudo chmod 755 -R "$DISK/Public"
-sudo chown alejandro -R "$DISK/Public"
-sudo chgrp alejandro -R "$DISK/Public"
+sudo chmod 755 -R "$DISK/data"
+sudo chown alejandro -R "$DISK/data"
+sudo chgrp alejandro -R "$DISK/data"
