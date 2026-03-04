cd87ed0f68bf9bafe7805dd91758634517e4ae1c
Alejandro Santiago <alemures@hotmail.com>
2026-03-04 17:29:50 +0100

    Improved set data permission script

--- scripts/set-data-permissions.sh
+++ scripts/set-data-permissions.sh
@@ -2,6 +2,9 @@
 
 DISK="/srv/dev-disk-by-uuid-c497dd5c-e3cc-48fb-b67c-a6cb0865cf53"
 
-sudo chmod 755 -R "$DISK/data"
+#sudo chmod 755 -R "$DISK/data"
+sudo find "$DISK/data" -type d -exec chmod 755 {} +
+sudo find "$DISK/data" -type f -exec chmod 644 {} +
+
 sudo chown alejandro -R "$DISK/data"
 sudo chgrp alejandro -R "$DISK/data"
